2283744aea7594b9c2b3c04de8953f7cfcfe1cbd
NatalieJoan <nhelak2001@gmail.com>
2025-05-16 23:22:59 +0200

    Add form field for contact

--- lib/contact.dart
+++ lib/contact.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/styles/gradient_layout.dart';
+import 'package:portfolio/styles/responsive_content.dart';
 import 'package:portfolio/widgets/app_bar_widget.dart';
+import 'package:portfolio/widgets/form_field_widget.dart';
 import 'package:portfolio/widgets/menu_nawigation.dart';
+import 'package:portfolio/widgets/social_media_widget.dart';
 
 class Contact extends StatelessWidget {
   const Contact({super.key});
@@ -12,10 +15,26 @@ class Contact extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppBarWidget(title: 'Contact'),
-        body: Row(
-          children: [
-            MenuNawigation()
-          ],
+        body: ResponsiveContent(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              MenuNawigation(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      SocialMediaWidget(label: 'Linkedin', url: 'https://www.linkedin.com/', iconPath: 'assets/icons/contact/Linkedin_black.png'),
+                      SocialMediaWidget(label: 'Linkedin', url: 'https://www.linkedin.com/', iconPath: 'assets/icons/contact/Github_black.png'),
+                      SocialMediaWidget(label: 'Linkedin', url: 'https://www.linkedin.com/', iconPath: 'assets/icons/contact/Gitlab_black.png'),
+                    ],
+                  ),
+                  FormFieldWidget(),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/form_field_widget.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/widgets/submit_button_widget.dart';
+
+class FormFieldWidget extends StatefulWidget {
+  const FormFieldWidget({super.key});
+
+  @override
+  State<FormFieldWidget> createState() => _FormFieldWidgetState();
+}
+
+class _FormFieldWidgetState extends State<FormFieldWidget> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 80,
+            width: 500,
+            child: TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter Email',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter an email';
+                }
+                return null;
+              },
+            ),
+          ),
+          const SizedBox(height: 12),
+          SizedBox(
+            width: 500,
+            child: TextFormField(
+              maxLines: 5,
+              keyboardType: TextInputType.multiline,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter your message',
+              ),
+              validator:(value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a message';
+                }
+                return null;
+              },
+            ),
+          ),
+          const SizedBox(height: 16),
+          SubmitButtonWidget(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Processing data'))
+                );
+              }
+            },
+            label: 'Send',
+          ),
+        ],
+      ),
+    );
+  }
+}
